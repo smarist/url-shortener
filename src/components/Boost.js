2d11c97e58228171811b9e9fef4df5e022c831ab
smarist <stmarisnwokike@gmail.com>
2022-05-31 00:36:43 +1200

    made some changes

--- src/components/Boost.js
+++ src/components/Boost.js
@@ -1,12 +1,19 @@
 import React from "react"
+import bgMobile from "../images/bg-boost-mobile.svg"
+import bgDesktop from "../images/bg-boost-desktop.svg"
 
 export default function Boost(){
     return(
         <section className="boost">
-            <h3>Boost your links today</h3>
-            <section className="section-boost">
-                <button className="button">Get started</button>
-            </section>
+            <picture>
+                <source media="(min-width: 768px)" srcSet={bgDesktop} />
+                <img src={bgMobile} alt=""/>
+            </picture>
+
+            <div className="flex-col boost-inner"> 
+               <h3>Boost your links today</h3>
+               <button className="button">Get started</button>
+            </div>
         </section>
     )
 
